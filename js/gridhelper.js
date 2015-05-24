9831389ba57b763bc690949048e4d002db639555
Oliver Steiner <oliver@mollo.ch>
2015-05-24 20:47:28 +0200

    Show Computed Classes - begin

--- js/gridhelper.js
+++ js/gridhelper.js
@@ -9,6 +9,7 @@
 
 function GridHelper() {
     this.viewport = 0;
+    this.on = 0;
 }
 
 
@@ -16,15 +17,14 @@ function GridHelper() {
 // ======================
 
 GridHelper.prototype.init = function () {
-    //  console.log("init gridHelper");
-    $("#develop").addClass('active');
-    $("#develop-start").hide();
 
+    this.addGripHelperMonitor();
     this.viewPort();
     this.markColumns();
     this.addColPanel();
     this.initColPanel();
 
+    this.on = true;
 
 };
 
@@ -37,7 +37,7 @@ GridHelper.prototype.viewPort = function () {
     //  console.log(width);
 
     if (width <= 480) {
-        // XXS - GREEN
+        // XXS - violet
         // Landscape phones and smaller */
         // max-width: 480px
         this.viewport = 'xxs';
@@ -67,7 +67,7 @@ GridHelper.prototype.viewPort = function () {
     }
 
     else if (width >= 1200) {
-        // LG - Violet
+        // LG - green
         // Large desktops and laptops
         // min-width: 1200px
         this.viewport = 'lg';
@@ -113,11 +113,12 @@ GridHelper.prototype.addColPanel = function () {
 
 // init InfoPanel Col
 // must be done for every change of Viewport
-// but must store manualy changes values
+// but must store manually changes values
 // ======================
 
 GridHelper.prototype.initColPanel = function () {
     this.resetInfoPanelCol();
+    this.initColPanelCode();
 
 
     // find all divs with current viewport
@@ -138,11 +139,6 @@ GridHelper.prototype.initColPanel = function () {
             elem.html(col_number);
             gridhelper.addInputs(elem, col_number, false);
         }
-        else{
-            console.log("leer");
-        }
-
-
 
 
     });
@@ -165,14 +161,53 @@ GridHelper.prototype.initColPanel = function () {
         if (isNaN(col_number)) {
             col_number = 0
         }
-        ;
-
 
         var elem_offset = $(this).children().children('.ghb-info-offset').html(col_number);
         elem_offset.html(col_number);
         gridhelper.addInputs(elem_offset, col_number, true);
 
     });
+
+
+};
+
+// show computed classes
+// ======================
+
+GridHelper.prototype.initColPanelCode = function (id) {
+    var html = "col-xs-3";
+
+
+    var all_col = $("[class*=col-]");
+
+    $.each(all_col, function(){
+
+        var all_class_names = $(this).attr( "class" );
+
+        console.log(all_class_names);
+// put all classes in array
+        var res = all_class_names.trim();
+        var res = res.split(" ");
+
+        console.log(res);
+
+        // remove temp gridhelper-classes
+        var remove = ['ghb-col'];
+
+
+        // add Code to Panel
+        $(this).children().children('.ghb-info-code').html(test);
+
+    })
+
+
+    function removeArrValue(arr,value){
+        var index = arr.indexOf(value);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+        return arr;
+    }
 
 
 }
@@ -186,15 +221,15 @@ GridHelper.prototype.resetInfoPanelCol = function () {
     $('.ghb-info-offset').html(0);
 
 
-    $('.ghb-info-col').each(function() {
+    $('.ghb-info-col').each(function () {
         gridhelper.addInputs($(this), 0, false);
     });
 
-    $('.ghb-info-offset').each(function() {
+    $('.ghb-info-offset').each(function () {
         gridhelper.addInputs($(this), 0, true);
     });
 
-}
+};
 
 
 // init Input:selects on InfoPanel
@@ -202,13 +237,13 @@ GridHelper.prototype.resetInfoPanelCol = function () {
 
 GridHelper.prototype.addInputs = function (elem, col_number, offset) {
 
-    var randomnumber = Math.floor((Math.random() * 10000) + 1);
-    var id = "input_" + randomnumber;
+    var random_number = Math.floor((Math.random() * 10000) + 1);
+    var id = "input_" + random_number;
     var input_select = this.inputElemSelect('ghb-select', col_number, id, offset);
 
     elem.html(input_select);
-    
-}
+
+};
 
 
 // inputElemSelect
@@ -232,72 +267,138 @@ GridHelper.prototype.inputElemSelect = function (class_name, col_number, id, off
     html = html + "</select>";
 
     return html;
-}
-
-
+};
 
 
 // inputElemSelect
 // ======================
 
-GridHelper.prototype.updateColNumber = function ( id, offset) {
+GridHelper.prototype.updateColNumber = function (id, offset) {
 
-    if(true === offset){ offset = "offset-"} else{ offset = ""}
+    if (true === offset) {
+        offset = "offset-"
+    } else {
+        offset = ""
+    }
 
     var elem = $("#" + id);
     var col_number = elem.val();
     var newClass = "col-" + this.viewport + "-" + offset + col_number;
 
-    console.log("elem_id= " + id);
-    console.log("viewport= " + this.viewport);
-    console.log("newClass=" + newClass);
+    // console.log("elem_id= " + id);
+    // console.log("viewport= " + this.viewport);
+    // console.log("newClass=" + newClass);
 
     $(elem).parent().parent().parent().removeClass(function (i, c) {
         var regex = new RegExp("col-" + gridhelper.viewport + "-" + offset + "[0-9+]{1,2}");
         var m = c.match(regex);
-        console.log("oldClass = "+ m);
+        // console.log("oldClass = " + m);
         return m ? m[0] : m
-    })
+    });
 
     $(elem).parent().parent().parent().addClass(newClass);
 
 
+};
+
+// hide
+// ======================
+
+GridHelper.prototype.hide = function () {
+    var all_col = $("[class*=col-]");
+    all_col.removeClass('ghb-col'); // GridHelperBootstrap-column
+
+    var all_panels = $(".ghb-infopanel");
+    all_panels.hide();
+    $('#responsive-status').hide();
+
+    this.on = false;
+}
+
+// show
+// ======================
+
+GridHelper.prototype.show = function () {
+    var all_col = $("[class*=col-]");
+    all_col.addClass('ghb-col'); // GridHelperBootstrap-column}
+    var all_panels = $(".ghb-infopanel");
+
+    all_panels.show();
+    $('#responsive-status').show();
+    this.on = true;
+}
+
+
+// toggle
+// ======================
+
+GridHelper.prototype.toggle = function () {
+
+    if ( this.on === true ) {
+        this.hide();
+    } else if ( this.on === false ) {
+        this.show();
+    }
 
 }
 
 
-function inputElemSelect(c, s, id, offset) {
-    var html = null;
-    var selected = null;
-    if (offset) {
-        c = c + "-offset"
-    }
-    html = "<select onchange = changeColNumber('" + id + "') class='" + c + "' id='" + id + "'>";
+// inputElemSelect
+// ======================
 
-    for (var i = 0; i <= 12; i++) {
-        if (s == i) {
-            selected = "selected";
-        }
-        else {
-            selected = null;
-        }
-        html = html + "<option " + selected + " value = '" + i + "'>" + i + "</option>";
-    }
-    html = html + "</select>";
+GridHelper.prototype.addGripHelperMonitor = function () {
+    var html = "<div id='gridhelper-monitor'>"
+        + "<a id='gridhelper-monitor-button'>GridHelper</a>"
+        + "<div id='responsive-status'></div>"
+        + "</div>";
 
-    return html;
+    $('body').append(html);
+
 }
+
+
+// Init Class
+// ======================
 
 
 $(document).ready(function () {
 
-    gridhelper = new GridHelper('Kitty', 'Meow');
+    gridhelper = new GridHelper();
     gridhelper.init();
+
+    $('#gridhelper-monitor-button').click( function(){
+        console.log('click');
+        gridhelper.toggle();
+    })
 });
 
 $(window).resize(function () {
-
     gridhelper.viewPort();
     gridhelper.initColPanel();
 
+});
+
+
+
+// http://stackoverflow.com/questions/5767325/remove-specific-element-from-an-array
+Object.defineProperty(Array.prototype, "removeItem", {
+    enumerable: false,
+    value: function (itemToRemove) {
+        // Count of removed items
+        var removeCounter = 0;
+        // Iterate every array item
+        for (var index = 0; index < this.length; index++) {
+            // If current array item equals itemToRemove then
+            if (this[index] === itemToRemove) {
+                // Remove array item at current index
+                this.splice(index, 1);
+
+                // Increment count of removed items
+                removeCounter++;
+                index--;
+            }
+        }
+        // Return count of removed items
+        return removeCounter;
+    }
 });
