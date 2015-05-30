10a6cb079c38aaa8723c885cd9b0de1c79421a0f
Oliver Steiner <oliver@mollo.ch>
2015-05-30 11:32:55 +0200

    refactoring

--- js/gridhelper.js
+++ js/gridhelper.js
@@ -23,7 +23,8 @@ $(document).ready(function () {
 // update the information on resizing
 $(window).resize(function () {
     gridhelper.viewPort();
-    gridhelper.initColPanel();
+    gridhelper.updateColPanel();
+
 });
 
 
@@ -37,6 +38,7 @@ function GridHelper(options) {
     this.on_popover = 0;
     this.options = options;
     this.modus = "col";
+    this.cols = 0;
 }
 
 
@@ -97,7 +99,6 @@ GridHelper.prototype.viewPort = function () {
         // Landscape phones and smaller */
         // max-width: 480px
         this.viewport = 'xxs';
-        $('gridhelper-popover-button').addr('class', 'eas');
     }
 
 
@@ -141,7 +142,7 @@ GridHelper.prototype.viewPort = function () {
 
     return this.viewport;
 
-}
+};
 
 
 // Mark Columns
@@ -167,11 +168,11 @@ GridHelper.prototype.addColPanel = function () {
     // foreach col take all classes and show on Panel
     $.each(all_col, function () {
 
-        // generate ID
-        var random_number = Math.floor((Math.random() * 10000) + 1);
-        var id = "bsh-" + random_number;
+        ++gridhelper.cols;
+        var id = "ghb-" + gridhelper.cols;
 
-        var info_panel = '<div class="ghb-infopanel" id="' + id + '">'
+        var info_panel = '<div class="col-active"></div>'
+            + '<div class="ghb-infopanel" id="' + id + '">'
             + '<div class="ghb-select ghb-info-col" data-toggle="tooltip" data-placement="top" data-original-title="col"></div>'
             + '<div class="ghb-select ghb-info-offset" data-toggle="tooltip" data-placement="top" data-original-title="offset"></div>'
             + '<div class="ghb-select ghb-info-push" data-toggle="tooltip" data-placement="top" data-original-title="push"></div>'
@@ -180,93 +181,41 @@ GridHelper.prototype.addColPanel = function () {
             + '<div class="ghb-info-code"></div>'
             + '</div>';
 
-        // console.log(this);
         // add the infoPanel to each div.row >div
         $(this).prepend(info_panel);
+        // add the Popover who shows the ClassNames
+        gridhelper.addPopover(id);
 
-        // add inputselect
+    });
+    // add all Input-elements
+    this.updateColPanel();
+};
+
+
+
+// ======================
+
+GridHelper.prototype.updateColPanel = function () {
+
+    var allcols = gridhelper.cols;
+
+    for (i = 1; i < allcols; i++) {
+
+        var id = "ghb-" + i;
+
+        // add input Element Select
         gridhelper.addInputs(id, 'col');
         gridhelper.addInputs(id, 'offset');
         gridhelper.addInputs(id, 'push');
         gridhelper.addInputs(id, 'pull');
 
-        // add popover
-        gridhelper.addPopover(id);
-
-    })
-
-
-    // add ID to infopanel
-};
-
-
-// init InfoPanel Col
-// must be done for every change of Viewport
-// but must store manually changes values
-// ======================
-
-GridHelper.prototype.initColPanel = function () {
-
-    this.resetInfoPanelCol();
-
-    // find all divs with current viewport
-    var regex = new RegExp("col-" + this.viewport + "-([0-9+]{1,2})");
-    var regex_offset = new RegExp("col-" + this.viewport + "-offset-([0-9+]{1,2})");
-
-    // col
-    var all_cols_with_viewport = $(".ghb-col").filter(function () {
-        return ((" " + this.className + " ").match(regex) != null);
-    });
-
-    $.each(all_cols_with_viewport, function () {
-
-        var class_name = (" " + $(this).attr('class') + " ").match(regex);
-        if (class_name) {
-            var col_number = parseInt(class_name[1], 10);
-            var elem = $(this).children().children('.ghb-info-col').html(col_number);
-
-            var id = $(this).children().first().attr('id');
-            // console.log(id);
-            elem.html(col_number);
-            gridhelper.addInputs(elem, col_number, 'col', id);
-        }
-
-
-    });
-
-
-    // col-offset
-    var all_cols_with_viewport_offset = $(".ghb-col").filter(function () {
-        return ((" " + this.className + " ").match(regex_offset) != null);
-    });
-
-    $.each(all_cols_with_viewport_offset, function () {
-
-        var class_names = (" " + $(this).attr('class') + " ").match(regex_offset);
-        if (class_names) {
-            var col_number = parseInt(class_names[1], 10);
-        }
-
-        //  console.log($(this).children().children());
-        //  console.log(col_number);
-        if (isNaN(col_number)) {
-            col_number = 0
-        }
-
-        var elem_offset = $(this).children().children('.ghb-info-offset').html(col_number);
-        elem_offset.html(col_number);
-
-        var id = $(this).children().first().attr('id');
-        //  gridhelper.addInputs(elem_offset, col_number, 'offset', id);
-
-    });
-
-
+    }
 };
 
 
 // show computed classes
 // ======================
+
 GridHelper.prototype.getColNumber = function (id, modus) {
     var col_number = "0";
 
@@ -313,7 +262,7 @@ GridHelper.prototype.getColNumber = function (id, modus) {
 
     return col_number;
 
-}
+};
 
 
 // show computed classes
@@ -330,7 +279,7 @@ GridHelper.prototype.addPopover = function (id) {
         var html = '<a tabindex="0" ' +
             'id="#popover_' + id + '"' +
             'role="button" ' +
-            'onclick="gridhelper.updateClassNames(\''+ id +'\');" ' +
+            'onclick="gridhelper.updatePopover(\'' + id + '\');" ' +
             'rel="gh-popover" ' +
             'class="btn-small"' +
             'data-toggle="popover" ' +
@@ -356,6 +305,7 @@ GridHelper.prototype.addPopover = function (id) {
 
 };
 
+
 // show computed classes
 // ======================
 
@@ -365,16 +315,7 @@ GridHelper.prototype.updateClassNames = function (id) {
     $('#popover_data_' + id).html(html);
 
 
-}
-// show computed classes
-// ======================
-
-GridHelper.prototype.updatePopover = function (id) {
-
-    var html = this.getClassNames(id);
-    $('#' + id).children('.ghb-info-code').children().children().children().html(html);
-
-}
+};
 
 
 // show computed classes
@@ -420,7 +361,38 @@ GridHelper.prototype.getClassNames = function (id) {
 
     return html_names;
 
-}
+};
+
+// show computed classes
+// ======================
+
+GridHelper.prototype.removeClassNames = function (id, modus) {
+
+    if (modus == "col") {
+        modus = ""
+    } else {
+        modus = modus + "-"
+    }
+
+    var class_name = '';
+    var root = $("#" + id).parent();
+
+    var regex = new RegExp("col-" + gridhelper.viewport + "-" + modus + "[0-9+]{1,2}");
+    var search_for_class = new RegExp(regex);
+
+    var classes = root.attr("class").split(' ');
+
+    for (var i = 0; i < classes.length; i++) {
+        if (classes[i].match(search_for_class)) {
+            class_name += classes[i] + ' ';
+        }
+    }
+
+    $(root).removeClass(class_name);
+
+    return class_name;
+
+};
 
 
 // reset InfoPanel Col
@@ -524,21 +496,26 @@ GridHelper.prototype.updateColNumber = function (id, mod) {
     //  console.log("viewport = " + this.viewport);
     //  console.log("root =" + root.attr('class'));
 
-    root.removeClass(function (index, css) {
-        // col-md-offset-4
-        var regex = new RegExp("col-" + gridhelper.viewport + "-" + gridhelper.modus + "[0-9+]{1,2}");
-        // console.log(regex);
-        var m = css.match(regex);
-        return m ? m[0] : m
-        // console.log("oldClass = " + m);
-
-    });
+    this.removeClassNames(id, modus);
 
     root.addClass(newClass);
     //  console.log("- - - -  - " );
-
-
     this.updatePopover(id);
+
+
+};
+// show computed classes
+// ======================
+
+GridHelper.prototype.updatePopover = function (id) {
+
+    var html = this.getClassNames(id);
+
+    $('#popover_data_' + id).html(html);
+    $('#' + id).children('.ghb-info-code').children().children('.popover-content').html(html);
+
+    return html;
+
 
 };
 
@@ -681,7 +658,7 @@ GridHelper.prototype.addGripHelperMonitor = function () {
 GridHelper.prototype.addToolTip = function () {
 
     $(".tip-top").tooltip({placement: 'top', title: 'test'});
-}
+};
 
 
 // http://stackoverflow.com/questions/5767325/remove-specific-element-from-an-array
